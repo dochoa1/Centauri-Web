c345057a3211800ae1c0f7e5cb509004639e01da
Dochoa1 <dochoa@macalester.edu>
2017-02-25 04:20:05 -0600

    Adds all of the authors submission to table

--- app.js
+++ app.js
@@ -18,15 +18,22 @@ var usersDatabase = database.ref().child("/research-examples/"); //Querying this
 
 var mySubmissions = usersDatabase.orderByChild("contributor-name").equalTo(contributorName); //only taking the papers made by this contributor
 
-submissionTitles = [];
+submissions = []; //Array to hold the information for all the pages the user has submitted
 
 function fillSubmissions(){
     //Function to fill submissionTitles array full of all the papers written by this contribtor
     mySubmissions.on('value', function(snapshot) {
         snapshot.forEach(function(item) {
-            var itemVal = item.val().content.title;
-            submissionTitles.push(itemVal);
-            console.log(submissionTitles[0])
+            //Iterate over all submissions by this author
+            var title = item.val().content.title;
+            var likes = item.val()['num-saves']
+            var dislikes = item.val()['num-ignores']
+            submissions.push({ //pushing an object of title, likes, and dislikes to array
+                title: title,
+                numLikes: likes,
+                numIgnores: dislikes
+            });
+            console.log(submissions[0].title)
         });
     });
 }
@@ -47,8 +54,22 @@ function fillSubmissions(){
 $(document).ready(function() {
     fillSubmissions();
     setTimeout(function() {
-        console.log(submissionTitles[0])
-        $("#sub").html(submissionTitles[0])
-        $("#user").html(contributorName);
-    }, 1000); //Time out because asynchronous
+        console.log(submissions[0].title)
+        var numSubmissions = submissions.length;
+        for (var i = 0; i < numSubmissions; i++){ //Over all submissions add them to a table
+            $("#submissionTable").find('tbody')
+                .append($('<tr>')
+                    .append($('<td>')
+                        .text(submissions[i].title)
+                    )
+                    .append($('<td>')
+                        .text(submissions[i].numLikes)
+                    )
+                    .append($('<td>')
+                        .text(submissions[i].numIgnores)
+                    )
+                )
+        }
+        $("#user").html(contributorName);  //Setting the users name
+    }, 2000); //Time out because asynchronous
 });
